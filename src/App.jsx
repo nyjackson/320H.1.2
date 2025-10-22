49676023ce757f182830d30ab901e0cbd87e3ff2
Nyala Jackson <nymjackson@gmail.com>
2025-10-22 15:44:55 -0400

    Trying to fix photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Article from './components/Article'
+import photo1 from '../blog-image-1.jpg'
+import photo2 from '../blog-image-2.jpg'
 import './App.css'
 
 function App() {
@@ -10,9 +12,9 @@ function App() {
     <>
     <Header />
     <Article date = "11/12/20" name = "On the Steet in Brooklyn" content ="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam qui facilis ipsam. Distinctio eaque at, doloremque ea doloribus sint corrupti dicta repellendus molestias natus expedita harum odio eveniet recusandae ad."
-    imgSrc = "./blog-image-1.jpg" imagAlt = "blog-image-1.jpg"/>
+    imgSrc = {photo1} imagAlt = "blog-image-1.jpg"/>
     <Article date = "11/11/20" name = "Vintage in Vogue" content ="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam qui facilis ipsam. Distinctio eaque at, doloremque ea doloribus sint corrupti dicta repellendus molestias natus expedita harum odio eveniet recusandae ad."
-    imgSrc = "./blog-image-2.jpg" imagAlt = "blog-image-2.jpg"/>
+    imgSrc = {photo2} imagAlt = "blog-image-2.jpg"/>
     <Footer />
     </>
   )
